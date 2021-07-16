0173ad7de01b3425c387d815f4d1bc09dd8cf97b
anthonybartczak <antoni.bartczak.w@gmail.com>
2021-07-16 19:11:48 +0200

    Added music commands

--- bot.py
+++ bot.py
@@ -1,20 +1,82 @@
-from typing import Text
 import discord
 from discord.ext import commands
-from os import environ
+from os import environ, listdir, rename, path, remove
+import youtube_dl
 
-bot = commands.Bot(command_prefix='>')
+client = commands.Bot(command_prefix='>')
 
 BOT_TOKEN = environ.get('BOT_TOKEN')
 
-@bot.command()
+@client.command()
 async def ping(ctx):
     await ctx.send('pong')
 
 
-@bot.command()
+@client.command()
 async def clear(ctx, amount: int):
     deleted = await ctx.channel.purge(limit=amount)
     await ctx.send(f"Deleted {len(deleted)} messages")
 
-bot.run(BOT_TOKEN)
+@client.command()
+async def play(ctx, url : str):
+    song_there = path.isfile("song.mp3")
+    try:
+        if song_there:
+            remove("song.mp3")
+    except PermissionError:
+        await ctx.send("Wait for the current playing music to end or use the 'stop' command")
+        return
+
+    voiceChannel = discord.utils.get(ctx.guild.voice_channels, name='General')
+    await voiceChannel.connect()
+    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+    }
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+    for file in listdir("./"):
+        if file.endswith(".mp3"):
+            rename(file, "song.mp3")
+    voice.play(discord.FFmpegPCMAudio("song.mp3"))
+
+
+@client.command()
+async def leave(ctx):
+    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+    if voice.is_connected():
+        await voice.disconnect()
+    else:
+        await ctx.send("The bot is not connected to a voice channel.")
+
+
+@client.command()
+async def pause(ctx):
+    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+    if voice.is_playing():
+        voice.pause()
+    else:
+        await ctx.send("Currently no audio is playing.")
+
+
+@client.command()
+async def resume(ctx):
+    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+    if voice.is_paused():
+        voice.resume()
+    else:
+        await ctx.send("The audio is not paused.")
+
+
+@client.command()
+async def stop(ctx):
+    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+    voice.stop()
+
+client.run(BOT_TOKEN)
