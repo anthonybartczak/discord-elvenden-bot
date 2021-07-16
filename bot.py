da7b29d96e9f3f7cb54244beb33476a5eedeba2c
anthonybartczak <antoni.bartczak.w@gmail.com>
2021-07-16 17:34:41 +0200

    Added clear command

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+from typing import Text
 import discord
 from discord.ext import commands
 from os import environ
@@ -9,5 +10,10 @@ BOT_TOKEN = environ.get('BOT_TOKEN')
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
+
+
+@bot.command()
+async def clear(ctx):
+    await ctx.channel.purge()
 
 bot.run(BOT_TOKEN)
