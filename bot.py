43c5221d015b51c4871faaacdeb208dc4ba25817
anthonybartczak <to0oni456@gmail.com>
2021-07-18 12:26:08 +0200

    Additional fixes to YT player

--- bot.py
+++ bot.py
@@ -17,18 +17,8 @@ async def clear(ctx, amount: int):
     await ctx.send(f"Deleted {len(deleted)} messages")
 
 
-# @client.command()
-# async def join(ctx):
-#     channel = ctx.message.author.voice.channel
-#     voice = get(client.voice_clients, guild=ctx.guild)
-#     if voice and voice.is_connected():
-#         await voice.move_to(channel)
-#     else:
-#         voice = await channel.connect()
-
-
 @client.command()
-async def play(ctx, url):
+async def play(ctx, url: str):
     channel = ctx.message.author.voice.channel
     voice = get(client.voice_clients, guild=ctx.guild)
     if voice and voice.is_connected():
@@ -36,15 +26,15 @@ async def play(ctx, url):
     else:
         voice = await channel.connect()
 
-    YDL_OPTIONS = {'format': 'bestaudio', 'noplaylist': 'True'}
-    FFMPEG_OPTIONS = {
-        'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
     voice = get(client.voice_clients, guild=ctx.guild)
 
     if not voice.is_playing():
+        YDL_OPTIONS = {'format': 'bestaudio', 'noplaylist': 'True'}
         with YoutubeDL(YDL_OPTIONS) as ydl:
             info = ydl.extract_info(url, download=False)
         URL = info['url']
+        FFMPEG_OPTIONS = {
+            'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
         voice.play(FFmpegPCMAudio(URL, **FFMPEG_OPTIONS))
         voice.is_playing()
         await ctx.send('Bot is playing')
