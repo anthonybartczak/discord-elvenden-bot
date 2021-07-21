564b0be9321e1c3b17066071fd194a30778c5d4f
anthonybartczak <antoni.bartczak.w@gmail.com>
2021-07-21 17:10:12 +0200

    This will fuck shit up. Proper version of the playlist command.

--- bot.py
+++ bot.py
@@ -7,10 +7,13 @@ from discord import FFmpegPCMAudio
 from youtube_dl import YoutubeDL
 from json import load
 import random
+import asyncio
 
 
 client = commands.Bot(command_prefix='>')
 client.remove_command('help')
+songs = asyncio.Queue()
+play_next_song = asyncio.Event()
 
 BOT_TOKEN = environ.get('BOT_TOKEN')
 
@@ -24,6 +27,7 @@ async def help(ctx):
     embed.add_field(name=">resume", value='Resume the music.', inline=True)
     embed.add_field(name=">pause", value='Pause the music.', inline=True)
     embed.add_field(name=">stop", value='Stop the music.', inline=True)
+    embed.add_field(name=">tracks", value='List the music currently available in the playlists.', inline=True)
     await ctx.send(embed=embed)
 
 @client.command()
@@ -41,6 +45,11 @@ async def reaction(ctx):
     zus_choice = random.choice(list(zus.items()))
     embed=discord.Embed(title='Zus reaction table', description='Zus is ' + zus_choice[1], color=0xb44141)
     embed.set_image(url=zus_choice[0])
+    await ctx.send(embed=embed)
+
+@client.command()
+async def tracks(ctx):
+    embed=discord.Embed(title='Tracks', description='Work in progress.', color=0xb44141)
     await ctx.send(embed=embed)
 
 @client.command()
@@ -83,7 +92,11 @@ async def play(ctx, url: str):
             'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
         voice.play(FFmpegPCMAudio(URL, **FFMPEG_OPTIONS))
         voice.is_playing()
-        await ctx.send('Bot is playing')
+        vid_name = info.get('title', None)
+        vid_id = info.get('id', None)
+        vid_info = '**Current track name:** \n' + vid_name + '\n\n' + '**URL**:\nhttps://www.youtube.com/watch?v=' + vid_id
+        embed=discord.Embed(title='Playing', description=vid_info, color=0xb44141)
+        await ctx.send(embed=embed)
 
     else:
         await ctx.send("Bot is already playing")
@@ -91,14 +104,15 @@ async def play(ctx, url: str):
 
 @client.command()
 async def playlist(ctx, choice: str):
+    server = ctx.message.server
     channel = ctx.message.author.voice.channel
     voice = get(client.voice_clients, guild=ctx.guild)
     if voice and voice.is_connected():
         await voice.move_to(channel)
     else:
         voice = await channel.connect()
-
     voice = get(client.voice_clients, guild=ctx.guild)
+
 
     if choice == 'town':
         chosen_list = playlists.TOWN_AMBIENCE
@@ -115,8 +129,16 @@ async def playlist(ctx, choice: str):
     elif choice == 'random':
         chosen_list = playlists.RANDOM_MUSIC
 
-    url = random.choice(chosen_list)
-
+    
+    queue_dict = dict.fromkeys(server.id, random.shuffle(chosen_list))
+    print(queue_dict)
+    queue_local =  songs[server.id]
+    playTrackFromPlaylist(ctx, queue_local)
+    
+    
+async def playTrackFromPlaylist(ctx, queue_local):
+    voice = get(client.voice_clients, guild=ctx.guild)
+    url = queue_local.pop(0)
     if not voice.is_playing():
         YDL_OPTIONS = {'format': 'bestaudio', 'noplaylist': 'True'}
         with YoutubeDL(YDL_OPTIONS) as ydl:
@@ -124,13 +146,12 @@ async def playlist(ctx, choice: str):
         URL = info['url']
         FFMPEG_OPTIONS = {
             'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
-        voice.play(FFmpegPCMAudio(URL, **FFMPEG_OPTIONS))
+        voice.play(FFmpegPCMAudio(URL, **FFMPEG_OPTIONS, after=lambda:playTrackFromPlaylist(ctx, queue_local)))
         voice.is_playing()
-        title = 'Playing the ' + choice + ' playlist'
         vid_name = info.get('title', None)
         vid_id = info.get('id', None)
         vid_info = '**Current track name:** \n' + vid_name + '\n\n' + '**URL**:\nhttps://www.youtube.com/watch?v=' + vid_id
-        embed=discord.Embed(title=title, description=vid_info, color=0xb44141)
+        embed=discord.Embed(title='Playing', description=vid_info, color=0xb44141)
         await ctx.send(embed=embed)
         
     else:
