fc6561f9f7bef6c583eec68bd641d56de7a3fd19
anthonybartczak <to0oni456@gmail.com>
2021-08-01 15:24:33 +0200

    Updated the help command

--- bot.py
+++ bot.py
@@ -1,4 +1,3 @@
-from asyncio import queues
 import playlists
 import discord
 from discord.ext import commands
@@ -8,10 +7,9 @@ from discord import FFmpegPCMAudio
 from youtube_dl import YoutubeDL
 from json import load
 import random
-from typing import Optional
 
 
-client = commands.Bot(command_prefix='>')
+client = commands.Bot(command_prefix='.')
 client.remove_command('help')
 
 def displayEmbedVideoInfo(name, id, thumbnail):
@@ -24,19 +22,27 @@ BOT_TOKEN = environ.get('BOT_TOKEN')
 
 @client.event
 async def on_ready():
-    activity = discord.Game(name=">help")
+    activity = discord.Game(name=".help")
     await client.change_presence(status=discord.Status.online, activity=activity)
 
 @client.command()
 async def help(ctx):
+    calc_help = \
+         'Poniżej krótka instrukcja dotycząca użytkowania polecenia **advance**:\n\n'\
+         'Format polecenia: \n\n'\
+         '**>advance** [c lub u] [wartość_początkowa] [wartość_docelowa] [talent*]\n\n\n'\
+         'c lub u ->  wybór pomiędzy rozwojem cechy lub umiejętności\n\n'\
+         'wartość_początkowa ->  początkowa wartość umiejętności lub cechy\n\n'\
+         'wartość_docelowa ->  docelowa wartość umiejętności lub cechy\n\n'\
+         'talent (opcjonalne) ->  jeśli któryś z talentów postaci obniża koszt rozwoju o 5 PD\n\n'\
+         'Przykłady:\n\n>advance c 5 12\n\n>advance u 12 16 t'
+
     embed=discord.Embed(title='Help', description='A short list of currently available commands:', color=0xb44141)
-    embed.add_field(name=">source", value='Display the source code.', inline=False)
-    embed.add_field(name=">clear [value]", value='Clear [value] text messages.', inline=False)
-    embed.add_field(name=">play [url]", value='Play music from a YouTube video.', inline=False)
-    embed.add_field(name=">resume", value='Resume the music.', inline=False)
-    embed.add_field(name=">pause", value='Pause the music.', inline=False)
-    embed.add_field(name=">stop", value='Stop the music.', inline=False)
-    embed.add_field(name=">tracks", value='List the music currently available in the playlists.', inline=False)
+    embed.add_field(name=".advance", value=calc_help, inline=False)
+    embed.add_field(name=".clear [value]", value='Wyczyść x wiadomości.', inline=False)
+    embed.add_field(name=".play [URL]", value='Odtwórz muzkę z YouTube (URL).', inline=False)
+    embed.add_field(name=".pause / .stop / .resume", value='Zapauzuj/zatrzymaj/wznów muzykę.', inline=False)
+
     await ctx.send(embed=embed)
 
 @client.command()
