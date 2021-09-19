6ab1d5fa8f4c30e378e718c54f0488fe0858cc17
anthonybartczak <to0oni456@gmail.com>
2021-09-19 12:19:06 +0200

    Music player embed

--- cogs/music.py
+++ cogs/music.py
@@ -9,6 +9,9 @@ from async_timeout import timeout
 from functools import partial
 from youtube_dl import YoutubeDL
 
+MAIN_COLOR = 0x8b54cf
+ERROR_COLOR = 0xff0000
+SUCCESS_COLOR = 0x16bd00
 
 ytdlopts = {
     'format': 'bestaudio/best',
@@ -69,7 +72,11 @@ class YTDLSource(discord.PCMVolumeTransformer):
             # take first item from a playlist
             data = data['entries'][0]
 
-        await ctx.send(f'```ini\n[Added {data["title"]} to the Queue.]\n```', delete_after=15)
+        #await ctx.send(f'```ini\n[Added {data["title"]} to the Queue.]\n```', delete_after=15)
+        
+        vid_info = '**Added** \n' + data["title"] + '\n\n' + '**URL**:\nhttps://www.youtube.com/watch?v=' + data["id"]
+        embed=discord.Embed(title='Playing', description=vid_info, color=MAIN_COLOR)
+        await ctx.send(embed=embed, delete_after=15)
 
         if download:
             source = ytdl.prepare_filename(data)
@@ -145,8 +152,13 @@ class MusicPlayer(commands.Cog):
             self.current = source
 
             self._guild.voice_client.play(source, after=lambda _: self.bot.loop.call_soon_threadsafe(self.next.set))
-            self.np = await self._channel.send(f'**Now Playing:** `{source.title}` requested by '
-                                               f'`{source.requester}`')
+            
+            vid_info = '**Added** \n' + {source.title} + '\n\n' + '**URL**:\nhttps://www.youtube.com/watch?v=' + {source.id}
+            embed=discord.Embed(title='Playing', description=vid_info, color=MAIN_COLOR)
+            embed.set_image(url={source.thumbnail})
+            
+            self.np = await self._channel.send(embed=embed)
+            
             await self.next.wait()
 
             # Make sure the FFmpeg process is cleaned up.
@@ -255,7 +267,7 @@ class Music(commands.Cog):
 
         await ctx.send(f'Connected to: **{channel}**', delete_after=20)
 
-    @commands.command(name='play', aliases=['sing'])
+    @commands.command(name='play', aliases=['sing', 'p'])
     async def play_(self, ctx, *, search: str):
         """Request a song and add it to the queue.
         This command attempts to join a valid voice channel if the bot is not already in one.
@@ -277,7 +289,7 @@ class Music(commands.Cog):
         # If download is False, source will be a dict which will be used later to regather the stream.
         # If download is True, source will be a discord.FFmpegPCMAudio with a VolumeTransformer.
         source = await YTDLSource.create_source(ctx, search, loop=self.bot.loop, download=False)
-
+        
         await player.queue.put(source)
 
     @commands.command(name='pause')
@@ -359,9 +371,12 @@ class Music(commands.Cog):
             await player.np.delete()
         except discord.HTTPException:
             pass
+        
+        vid_info = '**Now playing:** \n' + {vc.source.title} + '\n\n' + '**URL**:\nhttps://www.youtube.com/watch?v=' + {vc.source.id}
+        embed=discord.Embed(title='Playing', description=vid_info, color=MAIN_COLOR)
+        embed.set_image(url={vc.source.thumbnail})
 
-        player.np = await ctx.send(f'**Now Playing:** `{vc.source.title}` '
-                                   f'requested by `{vc.source.requester}`')
+        player.np = await ctx.send(embed=embed)
 
     @commands.command(name='volume', aliases=['vol'])
     async def change_volume(self, ctx, *, vol: float):
