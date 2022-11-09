7b60a3ba34caf2702d421b8e487a2b7f278633f7
anthonybartczak <to0oni456@gmail.com>
2022-11-09 17:31:30 +0100

    Slash commands with autocomplete 1.0

--- bot.py
+++ bot.py
@@ -21,7 +21,7 @@ SUCCESS_COLOR = 0x16bd00
 BOT_TOKEN = getenv('BOT_TOKEN')
 
 # Footer text (version + update date) for every single command.
-FOOTER_TEXT = 'Elvie v1.00 - WFRP 4ED\nOstatnia aktualizacja: 8/11/2022'
+FOOTER_TEXT = 'Elvie v1.00 - WFRP 4ED\nOstatnia aktualizacja: 9/11/2022'
 
 with open('content/talents.json', encoding="utf8") as jf:
     talents_json = load(jf)
@@ -30,6 +30,12 @@ talent_list = list(talents_json.keys())
 with open('content/abilities.json', encoding="utf8") as jf:
     abilities_json = load(jf)
 abilities_list = list(abilities_json.keys())
+
+advance_list = ["cechy", "umiejętności"]
+miscast_list = ["większa", "mniejsza"]
+corruption_list = ["zepsucie psychiczne", "spaczenie fizyczne"]
+advance_table_list = ["PC", "mobilna"]
+boolean_choice = ["tak", "nie"]
 
 # Discord intents declarations -> can be modified at https://discord.com/developers/
 intents = discord.Intents.default()
@@ -43,7 +49,7 @@ async def on_ready():
     await client.change_presence(status=discord.Status.online, activity=discord.Game(name=".help"))
     await tree.sync(guild=discord.Object(id=TEST_GUILD_ID))
 
-@tree.command(name = "servers", description = "Sprawdź do ilu serwerów jest połączony Elvie.")
+@tree.command(name = "serwery", description = "Sprawdź do ilu serwerów jest połączony Elvie.")
 async def servers(ctx):
     servers = list(client.guilds)
     description = 'Połączony z **' + str(len(servers)) + '** serwerami\n\n'
@@ -51,41 +57,39 @@ async def servers(ctx):
     embed.set_footer(text = FOOTER_TEXT, icon_url = pic.BOT_AVATAR)
     await ctx.response.send_message(embed=embed)
 
-@tree.command(name = "help", description = "Pokazuje instrukcję bota Elvie.")
+@tree.command(name = "pomoc", description = "Pokazuje instrukcję bota Elvie.")
 async def help(ctx):
     description = \
         'Poniżej znajdziesz listę obecnie dostępnych poleceń. Argumenty oznaczone `*` są opcjonalne:\n\n'\
-        '**advance <c/u> <start> <cel> <t*>**\nOblicz koszt rozwoju od `start` do `cel` cechy lub umiejętności (`c` lub `u`). Argument `t` obniża koszt rozwinięcia o 5 PD. Przykładowo:\n`.advance c 12 15` albo `.advance u 5 14 t`\n\n'\
-        '**advance_table <m*>**\nWyświetl tabelę *Koszt rozwoju cech i umiejętności w PD*. Argument `m` wyświetla tabelę w wersji na urządzenia mobilne. Przykładowo:\n`.advance_table` albo `.advance_table m`\n\n'\
-        '**talent <nazwa>**\nWyświetl opis talentu `nazwa`. Nazwa musi zostać podana z uwzględnieniem polskich znaków oraz bez użycia nawiasów. Przykładowo:\n`.talent bardzo szybki` albo `.talent magia tajemna`\n\n'\
-        '**ability <nazwa>**\nWyświetl opis umiejętności `nazwa`. Nazwa musi zostać podana z uwzględnieniem polskich znaków oraz bez użycia nawiasów. Przykładowo:\n`.ability rzemiosło` albo `.ability mocna głowa`\n\nPodziękowania dla Kazyleusz#2024.\n\n'\
-        '**miscast <w*>**\nWylosuj mniejszą lub większą (parametr `w`) manifestację. Przykładowo:\n`.miscast` albo `.miscast w`\n\n'\
-        '**corruption <p*>**\nWylosuj spaczenie fizyczne lub zepsucie psychiczne (parametr `p`). Przykładowo:\n`.corruption` albo `.corruption p`\n\n'\
-        '**fortune**\nWylosuj 4 karty, wybierz jedną i sprawdź czy `Ranald` wysłucha Twej modlitwy.\n\n'\
-        '**invite**\nWygeneruj `URL`, dzięki któremu będziesz mógł zaprosić Elviego na własny serwer.\n\n'\
+        '**rozwinięcie <cecha/umiejętność> <start> <cel> <t*>**\nOblicz koszt rozwoju od `start` do `cel` cechy lub umiejętności. Argument `talent` równy `tak` obniża koszt rozwinięcia o 5 PD.\n\n'\
+        '**tabela_rozwinięć <wersja>**\nWyświetl tabelę *Koszt rozwoju cech i umiejętności w PD*. Argument `mobilna` wyświetla tabelę w wersji na urządzenia mobilne.\n\n'\
+        '**talent <nazwa>**\nWyświetl opis talentu `nazwa`.\n\n'\
+        '**umiejętność <nazwa>**\nWyświetl opis umiejętności `nazwa`.\n\nPodziękowania dla Kazyleusz#2024.\n\n'\
+        '**manifestacja <mniejsza/większa>**\nWylosuj mniejszą lub większą (parametr `w`) manifestację.\n\n'\
+        '**spaczenie <fizyczne/psychiczne>**\nWylosuj spaczenie fizyczne lub zepsucie psychiczne (parametr `p`).\n\n'\
+        '**fortuna**\nWylosuj 4 karty, wybierz jedną i sprawdź czy `Ranald` wysłucha Twej modlitwy.\n\n'\
+        '**zaproszenie**\nWygeneruj `URL`, dzięki któremu będziesz mógł zaprosić Elviego na własny serwer.\n\n'\
 
     embed=discord.Embed(title='Krótka instrukcja bota Elvie', description=description, color=MAIN_COLOR)
     embed.set_footer(text = FOOTER_TEXT, icon_url = pic.BOT_AVATAR)
     await ctx.response.send_message(embed=embed)
 
-@tree.command(name = "invite", description = "Wygeneruj zaproszenie dla bota Elvie.")
+@tree.command(name = "zaproszenie", description = "Wygeneruj zaproszenie dla bota Elvie.")
 async def invite(ctx):
     embed=discord.Embed(title='Link do zaproszenia', description='https://discord.com/api/oauth2/authorize?client_id=864205486056669244&permissions=1084516330561&scope=bot%20applications.commands', color=MAIN_COLOR)
     embed.set_footer(text = FOOTER_TEXT, icon_url = pic.BOT_AVATAR)
     await ctx.response.send_message(embed=embed)
 
-@tree.command(name = "advance", description = "Oblicz koszt rozwinięcia cechy lub umiejętności.")
-async def advance(ctx, type: str, init: int, goal: int, talent: str=None):
+@tree.command(name = "rozwinięcie", description = "Oblicz koszt rozwinięcia cechy lub umiejętności.")
+async def advance(ctx, choice: str, init: int, goal: int, talent: str="nie"):
 
     ability_map = {5:10, 10:15, 15:20, 20:30, 25:40, 30:60, 35:80, 40:110, 45:140, 50:180, 55:220, 60:270, 65:320, 70:380, 9999:440}
     attribute_map = {5:25, 10:30, 15:40, 20:50, 25:70, 30:90, 35:120, 40:150, 45:190, 50:230, 55:280, 60:330, 65:390, 70:450, 9999:520}
 
-    if type == 'c':
+    if choice == 'cechy':
         chosen_map = attribute_map
-        choice = 'cechy'
-    elif type == 'u':
+    elif choice == 'umiejętności':
         chosen_map = ability_map
-        choice = 'umiejętności'
 
     current = init
     cost_sum = 0
@@ -100,7 +104,7 @@ async def advance(ctx, type: str, init: int, goal: int, talent: str=None):
     description = \
         'Twoja początkowa wartość **' + choice + '** to: **' + str(init) + '**\n'\
         'Twoja docelowa wartość **' + choice + '** to: **' + str(goal) + '**\n\n'
-    if talent == 't':
+    if talent == "tak":
         description += 'Jeden z Twoich talentów obniża koszt o **5 PD** za każde rozwinięcie.\n\n'\
         'Finalny koszt rozwinięcia to: **' + str(cost_sum - 5 * (goal - init)) + ' PD**'
     else:
@@ -111,47 +115,87 @@ async def advance(ctx, type: str, init: int, goal: int, talent: str=None):
 
     await ctx.response.send_message(embed=embed)
 
-@tree.command(name = "miscast", description = "Wylosuj mniejszą lub większą manifestację.")
-async def miscast(ctx, type: str='m'):
+@advance.autocomplete('choice')
+async def advance_autocomplete(
+    interaction: discord.Interaction,
+    current: str,
+    ) -> typing.List[discord.app_commands.Choice[str]]:
+        advance_list
+        return [
+            discord.app_commands.Choice(name=choice, value=choice)
+            for choice in advance_list if current.lower() in choice.lower()
+        ][:25]
+
+@advance.autocomplete('talent')
+async def advance_autocomplete(
+    interaction: discord.Interaction,
+    current: str,
+    ) -> typing.List[discord.app_commands.Choice[str]]:
+        boolean_choice
+        return [
+            discord.app_commands.Choice(name=choice, value=choice)
+            for choice in boolean_choice if current.lower() in choice.lower()
+        ]
+
+@tree.command(name = "manifestacja", description = "Wylosuj mniejszą lub większą manifestację.")
+async def miscast(ctx, choice: str):
     roll = random.randint(1,100)
 
-    if type == 'w':
+    if choice == 'większa':
         table = tab.MISCAST_MAJOR
-        name = 'Większa'
-    else:
+    elif choice == 'mniejsza':
         table = tab.MISCAST_MINOR
-        name = 'Mniejsza'
 
     for i, r in enumerate(range(5, 101, 5)):
         if roll <= r:
             miscast = table[i]
             break
 
-    embed=discord.Embed(title=name + ' manifestacja!', description='Wyrzuciłeś **' + str(roll) + '**...\n\n' + miscast, color=MAIN_COLOR)
+    embed=discord.Embed(title=choice.capitalize() + ' manifestacja!', description='Wyrzuciłeś **' + str(roll) + '**...\n\n' + miscast, color=MAIN_COLOR)
     embed.set_footer(text = FOOTER_TEXT, icon_url = pic.BOT_AVATAR)
     await ctx.response.send_message(embed=embed)
 
-@tree.command(name = "corruption", description = "Wylosuj spaczenie fizyczne lub psychiczne.")
-async def corruption(ctx, type: str='f'):
+@miscast.autocomplete('choice')
+async def miscast_autocomplete(
+    interaction: discord.Interaction,
+    current: str,
+    ) -> typing.List[discord.app_commands.Choice[str]]:
+        miscast_list
+        return [
+            discord.app_commands.Choice(name=choice, value=choice)
+            for choice in miscast_list if current.lower() in choice.lower()
+        ][:25]
+
+@tree.command(name = "spaczenie", description = "Wylosuj spaczenie fizyczne lub psychiczne.")
+async def corruption(ctx, choice: str):
     roll = random.randint(1,100)
 
-    if type == 'p':
+    if choice == 'zepsucie psychiczne':
         table = tab.CORRUPTION_MENTAL
-        name = 'zepsucie psychiczne!'
-    else:
+    elif choice == 'spaczenie fizyczne':
         table = tab.CORRUPTION_PHYSICAL
-        name = 'spaczenie fizyczne!'
 
     for i, r in enumerate(range(5, 101, 5)):
         if roll <= r:
             corruption = table[i]
             break
 
-    embed=discord.Embed(title='Wylosowano ' + name, description='Wyrzuciłeś **' + str(roll) + '**...\n\n' + corruption, color=MAIN_COLOR)
+    embed=discord.Embed(title='Wylosowano ' + choice, description='Wyrzuciłeś **' + str(roll) + '**...\n\n' + corruption, color=MAIN_COLOR)
     embed.set_footer(text = FOOTER_TEXT, icon_url = pic.BOT_AVATAR)
     await ctx.response.send_message(embed=embed)
 
-@tree.command(name = "fortune", description = "Wylosuj kartę i sprawdź czy Ranald przygląda się Twoim losom!")
+@corruption.autocomplete('choice')
+async def corruption_autocomplete(
+    interaction: discord.Interaction,
+    current: str,
+    ) -> typing.List[discord.app_commands.Choice[str]]:
+        corruption_list
+        return [
+            discord.app_commands.Choice(name=choice, value=choice)
+            for choice in corruption_list if current.lower() in choice.lower()
+        ][:25]
+
+@tree.command(name = "fortuna", description = "Wylosuj kartę i sprawdź czy Ranald przygląda się Twoim losom!")
 async def fortune(ctx):
     await ctx.response.send_message("Jak sobie życzysz...")
     reactions = ['1️⃣', '2️⃣', '3️⃣', '4️⃣']
@@ -188,14 +232,14 @@ async def fortune(ctx):
         await ctx.channel.send(embed=embed)
 
 
-@tree.command(name = "advance_table", description = "Wyświetl tabele rozwinięcia cech lub umiejętności.")
-async def advance_table(ctx, version: str='pc'):
-    if version == 'm':
+@tree.command(name = "tabela_rozwinięć", description = "Wyświetl tabele rozwinięcia cech lub umiejętności.")
+async def advance_table(ctx, version: str):
+    if version == 'mobilna':
         image = pic.ADVANCE_TABLE_PIC
         embed=discord.Embed(title='Koszt rozwoju cech i umiejętności w PD', description='', color=MAIN_COLOR)
         embed.set_footer(text = FOOTER_TEXT, icon_url = pic.BOT_AVATAR)
         embed.set_image(url=image)
-    else:
+    elif version == "PC":
         description = tab.ADV_TABLE
         embed=discord.Embed(title='Koszt rozwoju cech i umiejętności w PD', description=description, color=MAIN_COLOR)
         embed.set_footer(text = FOOTER_TEXT, icon_url = pic.BOT_AVATAR)
@@ -218,8 +262,19 @@ async def talent(ctx, *, talent_name: str):
 
     await ctx.channel.send(embed=embed)
 
+@advance_table.autocomplete('version')
+async def advance_table_autocomplete(
+    interaction: discord.Interaction,
+    current: str,
+    ) -> typing.List[discord.app_commands.Choice[str]]:
+        advance_table_list
+        return [
+            discord.app_commands.Choice(name=choice, value=choice)
+            for choice in advance_table_list if current.lower() in choice.lower()
+        ][:25]
+
 @talent.autocomplete('talent_name')
-async def fruits_autocomplete(
+async def talents_autocomplete(
     interaction: discord.Interaction,
     current: str,
     ) -> typing.List[discord.app_commands.Choice[str]]:
@@ -229,7 +284,7 @@ async def fruits_autocomplete(
             for talent in talent_list if current.lower() in talent.lower()
         ][:25]
 
-@tree.command(name = "ability", description = "Wyświetl opis umiejętności.")
+@tree.command(name = "umiejętność", description = "Wyświetl opis umiejętności.")
 async def ability(ctx, *, ability_name: str):
     ability_name = ability_name.replace(' ','_').lower()
 
@@ -246,6 +301,17 @@ async def ability(ctx, *, ability_name: str):
         embed.set_footer(text = FOOTER_TEXT, icon_url = pic.BOT_AVATAR)
 
     await ctx.channel.send(embed=embed)
+
+@ability.autocomplete('ability_name')
+async def abilities_autocomplete(
+    interaction: discord.Interaction,
+    current: str,
+    ) -> typing.List[discord.app_commands.Choice[str]]:
+        abilities_list
+        return [
+            discord.app_commands.Choice(name=ability, value=ability)
+            for ability in abilities_list if current.lower() in ability.lower()
+        ][:25]
 
 tree.copy_global_to(guild=discord.Object(id=TEST_GUILD_ID))
 
